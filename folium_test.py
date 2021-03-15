07651e730a359aaed90b52e9da539dcaef83ee60
OQinYuan <OQinYuan@dso.org.sg>
2021-03-15 17:58:17 +0800

    monday

--- folium_test.py
+++ folium_test.py
@@ -6,7 +6,8 @@ from folium import Choropleth, Circle, Marker
 from folium.plugins import HeatMap, MarkerCluster
 
 # Create a map
-m = folium.Map(location=[40.721, -73.8635], tiles='http://localhost:8080/styles/klokantech-basic/{z}/{x}/{y}.png',
+m = folium.Map(location=[40.721, -73.8635],
+               tiles='http://localhost:3650/api/tiles/2017-07-03_new-york_new-york/{z}/{x}/{y}.png',
                zoom_start=10, attr='Maps powered by OpenStreetMap data')
 
 area_data = pd.read_csv('ny_bay_AIS_2020_01_01.csv')
--- geofencing.py
+++ geofencing.py
@@ -1,9 +1,12 @@
 import pandas as pd
-#import geopandas as gpd
+import geopandas as gpd
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-#https://towardsdatascience.com/the-art-of-geofencing-in-python-e6cc237e172d
+# https://towardsdatascience.com/the-art-of-geofencing-in-python-e6cc237e172d
 
 df = pd.read_csv(r"C:\Users\HP\PycharmProjects\playground\go-track-trackPoints.csv")
 print(df.head())
+
+px.set_mapbox_access_token(r'pk.eyJ1Ijoic2hha2Fzb20iLCJhIjoiY2plMWg1NGFpMXZ5NjJxbjhlM2ttN3AwbiJ9.RtGYHmreKiyBfHuElgYq_w')
+px.scatter_mapbox(gdf, lat='latitude', lon='longitude', size_max=6, zoom=8, width=1200, height=800)
